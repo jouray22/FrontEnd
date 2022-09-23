04b31d60ff1cccd3857d60a69df372c95abb5366
Johana Uray <uray.johana18@gmail.com>
2022-09-23 18:48:22 -0300

    Configuracion Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { CompetenciasComponent } from './componentes/competencias/competencias.component';
+import { PortafolioService } from './servicios/portafolio.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    CompetenciasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/educacion/educacion.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { PortafolioService } from 'src/app/servicios/portafolio.service';
+
+@Component({
+  selector: 'app-educacion',
+  templateUrl: './educacion.component.html',
+  styleUrls: ['./educacion.component.css']
+})
+export class EducacionComponent implements OnInit {
+
+  educacionList:any;
+  constructor(private datosPortafolio:PortafolioService) { }
+
+  ngOnInit(): void {
+    this.datosPortafolio.obtenerDatos().subscribe(data=>{
+      this.educacionList=data.education;
+    })
+  }
+
+}
